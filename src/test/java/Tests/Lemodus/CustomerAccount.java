fb4d7f2eb503ba454070f74dcc8ac617ca760bdc
Vartotojas <Vilmaboa@gmail.com>
2022-10-14 17:52:25 +0300

    Add new class Customr account

--- src/test/java/Tests/Lemodus/CustomerAccount.java
+++ src/test/java/Tests/Lemodus/CustomerAccount.java
@@ -1,5 +1,31 @@
 package Tests.Lemodus;
 
-public class CustomerAccount {
+import Tests.TestBase;
+import lt.pages.Lemodus.CustomerAccountPage;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class CustomerAccount extends TestBase {
+    @BeforeMethod
+    @Override
+    public void setUp() {
+        super.setUp();
+        CustomerAccountPage.open("https://www.lemodus.lt/customer/account/index/");
+
+    }
+
+    @Test
+    private void searchField() {
+        String expectedMesage = "Suknele esprit casual";
+        String actualMesage = null;
+
+        CustomerAccountPage.clickSearchField();
+        CustomerAccountPage.enterProductSuknele(expectedMesage);
+        CustomerAccountPage.clickAutcoplete();
+
+        Assert.assertEquals(actualMesage,expectedMesage);
+
+    }
 
 }
