c81600004bd0a6fb6b56064e0ccfdc4a1332b241
Vartotojas <Vilmaboa@gmail.com>
2022-10-14 19:03:39 +0300

    Add TestLister clas

--- src/test/java/Tests/TestBase.java
+++ src/test/java/Tests/TestBase.java
@@ -3,7 +3,10 @@ package Tests;
 import lt.pages.Lemodus.HomePage;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import utils.TestListener;
 
+@Listeners(TestListener.class)
 public class TestBase {
 
     @BeforeMethod
